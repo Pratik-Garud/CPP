12a6f69bcf9bd89727b40b05cedc13eff065fbf2
Pratik-Garud <pratikgarud1208@gmail.com>
2024-11-28 03:08:22 +0530

    hey

--- dataTypes.c
+++ dataTypes.c
@@ -1,20 +1,20 @@
 //1)
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int intVal;
-//     char charVal;
-//     float floatVal;
-//     double doubleVal;
+int main() {
+    int intVal;
+    char charVal;
+    float floatVal;
+    double doubleVal;
 
-//     printf("Size of int: %zu bytes\n", sizeof(intVal));
-//     printf("Size of char: %zu byte\n", sizeof(charVal));
-//     printf("Size of float: %zu bytes\n", sizeof(floatVal));
-//     printf("Size of double: %zu bytes\n", sizeof(doubleVal));
+    printf("Size of int: %zu bytes\n", sizeof(intVal));
+    printf("Size of char: %zu byte\n", sizeof(charVal));
+    printf("Size of float: %zu bytes\n", sizeof(floatVal));
+    printf("Size of double: %zu bytes\n", sizeof(doubleVal));
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -22,20 +22,20 @@
 
 // 2)
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
+int main(){
 
-//     int num1=2;
-//     int num2=100;
+    int num1=2;
+    int num2=100;
 
-//     printf("Addition of two numbers is:%d \n",num1+num2);
-//     printf("Subtraction of two numbers is:%d \n",num1-num2);
-//     printf("Multiplication of two numbers is: %d\n",num1*num2);
-//     printf("division of two numbers is: %d\n",num1/num2);
+    printf("Addition of two numbers is:%d \n",num1+num2);
+    printf("Subtraction of two numbers is:%d \n",num1-num2);
+    printf("Multiplication of two numbers is: %d\n",num1*num2);
+    printf("division of two numbers is: %d\n",num1/num2);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -43,36 +43,36 @@
 
 // 3)
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
+int main(){
 
-//     int x=19;
-//     int y= x++ + --x - x++;
-//     int z= --x + x-- - x++ + ++x;
+    int x=19;
+    int y= x++ + --x - x++;
+    int z= --x + x-- - x++ + ++x;
      
-//     printf("%d\n",y);
-//     printf("%d\n",z);
+    printf("%d\n",y);
+    printf("%d\n",z);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
 // 4)
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
+int main(){
 
-//     printf("450 / 10: %d \n",450/10);
-//     printf("59632 modulo 10: %d \n",59632%10);
-//     printf("72 / 6: %d \n",72/8);
-//     printf("56 modulo 8: %d \n",56%8);
+    printf("450 / 10: %d \n",450/10);
+    printf("59632 modulo 10: %d \n",59632%10);
+    printf("72 / 6: %d \n",72/8);
+    printf("56 modulo 8: %d \n",56%8);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -84,19 +84,19 @@
 
 #include<stdio.h>
 
-// int main(){
-//     int num1=10;
-//     int num2=20;
+int main(){
+    int num1=10;
+    int num2=20;
 
-//     num2= num1 + num2;
-//     num1 = num2 - num1;
-//     num2 = num2- num1;
+    num2= num1 + num2;
+    num1 = num2 - num1;
+    num2 = num2- num1;
 
-//     printf("num1: %d\n",num1);
-//     printf("num2: %d\n",num2);
+    printf("num1: %d\n",num1);
+    printf("num2: %d\n",num2);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -105,17 +105,17 @@
 // 6)
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     int num=10;
+int main(){
+    int num=10;
 
-//     if(num>5){
-//         printf("number is grater than 5");
-//     }
+    if(num>5){
+        printf("number is grater than 5");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -123,17 +123,17 @@
 // 7)
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     int num=99;
+int main(){
+    int num=99;
 
-//     if(num<100){
-//         printf("number is less than 100");
-//     }
+    if(num<100){
+        printf("number is less than 100");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -145,19 +145,19 @@
 // 8)
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     int num=44;
+int main(){
+    int num=44;
 
-//     if(num%2==0){
-//         printf("number is even");
-//     }else{
-//         printf("number is odd");
-//     }
+    if(num%2==0){
+        printf("number is even");
+    }else{
+        printf("number is odd");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -167,21 +167,21 @@
 
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     int num=33;
+int main(){
+    int num=33;
 
-//     if(num<10){
-//         printf("number is less than 10");
-//     }else if(num>10 && num<20){
-//         printf("number is greater than 10 but less than 20");
-//     }else{
-//         printf("Happy coding");
-//     }
+    if(num<10){
+        printf("number is less than 10");
+    }else if(num>10 && num<20){
+        printf("number is greater than 10 but less than 20");
+    }else{
+        printf("Happy coding");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
